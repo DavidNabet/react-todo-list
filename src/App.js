c2d824727c77d0faa19164b3f0e1c8ffd5067f9e
DavidNabet <david97.nabet@gmail.com>
2021-04-30 18:03:57 +0200

    restart

--- src/App.js
+++ src/App.js
@@ -1,34 +1,68 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import Todo from "./components/Todo";
 import "./App.css";
 function App() {
-  const [task, setTask] = useState("");
-  const [nbTask, setNbTask] = useState([]);
+  const [nbTask, setNbTask] = useState([
+    { name: "Balancer la vieille", status: false },
+    { name: "Faire chier les gens", status: false },
+    { name: "Péter plus haut que son derrière", status: true },
+  ]);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    const value = task;
-    const tab = [...nbTask];
-    tab.push(value);
-    setNbTask(tab);
-    console.log(tab);
-    setTask("");
-  };
+  const [taskInput, setTaskInput] = useState("");
 
   return (
     <div className="container">
       <Header />
       <div className="form-group">
         {nbTask.map((todos, i) => {
-          return <Todo key={i} id={i} todos={todos} />;
+          return (
+            <div key={i}>
+              <input
+                type="checkbox"
+                defaultChecked={todos.status}
+                onClick={() => {
+                  const newTodos = [...nbTask];
+                  newTodos[i].status = !newTodos[i].status;
+                  setTaskInput(newTodos);
+                }}
+              />
+              <span
+                style={{
+                  textDecoration:
+                    todos.status === true ? "line-through" : "none",
+                }}
+              >
+                {todos.name}
+              </span>
+              <button
+                onClick={() => {
+                  const remove = [...nbTask];
+                  remove.splice(i, 1);
+                  setTaskInput(remove);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
         })}
-        <input
-          type="text"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <button onClick={handleClick}>Add task</button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const newTask = [...nbTask];
+            newTask.push({ name: taskInput, status: false });
+            setNbTask(newTask);
+            setTaskInput("");
+          }}
+        >
+          <input
+            placeholder="Ecrire une tâche"
+            type="text"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+          />
+          <input type="submit" value="Ajouter" />
+        </form>
       </div>
     </div>
   );
