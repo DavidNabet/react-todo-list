1a488f349feb91441742da1a08547e5384f80282
DavidNabet <43647871+DavidNabet@users.noreply.github.com>
2021-06-16 14:40:25 +0200

    me suis trompe

--- src/App.js
+++ src/App.js
@@ -2,70 +2,70 @@ import { useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
 function App() {
-  const [nbTask, setNbTask] = useState([
-    { name: "Balancer la vieille", status: false },
-    { name: "Faire chier les gens", status: false },
-    { name: "Péter plus haut que son derrière", status: true },
-  ]);
+	const [nbTask, setNbTask] = useState([
+		{ name: "Balancer la vieille", status: false },
+		{ name: "Faire chier les gens", status: false },
+		{ name: "Péter plus haut que son derrière", status: true },
+	]);
 
-  const [taskInput, setTaskInput] = useState("");
+	const [taskInput, setTaskInput] = useState("");
 
-  return (
-    <div className="container">
-      <Header />
-      <div className="form-group">
-        {nbTask.map((todos, i) => {
-          return (
-            <div key={i}>
-              <input
-                type="checkbox"
-                defaultChecked={todos.status}
-                onClick={() => {
-                  const newTodos = [...nbTask];
-                  newTodos[i].status = !newTodos[i].status;
-                  setTaskInput(newTodos);
-                }}
-              />
-              <span
-                style={{
-                  textDecoration:
-                    todos.status === true ? "line-through" : "none",
-                }}
-              >
-                {todos.name}
-              </span>
-              <button
-                onClick={() => {
-                  const remove = [...nbTask];
-                  remove.splice(i, 1);
-                  setTaskInput(remove);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          );
-        })}
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const newTask = [...nbTask];
-            newTask.push({ name: taskInput, status: false });
-            setNbTask(newTask);
-            setTaskInput("");
-          }}
-        >
-          <input
-            placeholder="Ecrire une tâche"
-            type="text"
-            value={taskInput}
-            onChange={(e) => setTaskInput(e.target.value)}
-          />
-          <input type="submit" value="Ajouter" />
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Header />
+			<div className="form-group">
+				{nbTask.map((todos, i) => {
+					return (
+						<div key={i}>
+							<input
+								type="checkbox"
+								defaultChecked={todos.status}
+								onClick={() => {
+									const newTodos = [...nbTask];
+									newTodos[i].status = !newTodos[i].status;
+									setNbTask(newTodos);
+								}}
+							/>
+							<span
+								style={{
+									textDecoration:
+										todos.status === true ? "line-through" : "none",
+								}}
+							>
+								{todos.name}
+							</span>
+							<button
+								onClick={() => {
+									const remove = [...nbTask];
+									remove.splice(i, 1);
+									setNbTask(remove);
+								}}
+							>
+								Delete
+							</button>
+						</div>
+					);
+				})}
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						const newTask = [...nbTask];
+						newTask.push({ name: taskInput, status: false });
+						setNbTask(newTask);
+						setTaskInput("");
+					}}
+				>
+					<input
+						placeholder="Ecrire une tâche"
+						type="text"
+						value={taskInput}
+						onChange={(e) => setTaskInput(e.target.value)}
+					/>
+					<input type="submit" value="Ajouter" />
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
